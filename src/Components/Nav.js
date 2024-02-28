eeb78ce74fc95bdd26e75e29fc1508cb8030455d
DimitryAujour <106028495+DimitryAujour@users.noreply.github.com>
2024-02-28 12:58:21 -0500

    Starting files

---
+++ src/Components/Nav.js
@@ -0,0 +1,10 @@
+import React  from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+<img src ='/images/logo.png' className={'Logo'}   alt={'Airbnb logo'}/>
+   </nav>
+  );
+}
+export default Nav;
